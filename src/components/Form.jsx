254df8a09f9887f19cb35b4c76cc8b991fee95f5
Maria del Mar Lamilla <lamilla767@gmail.com>
2022-05-04 02:56:44 -0500

    backend connected with fronend

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,15 +4,28 @@ import { Store } from './StoreProvider';
 const Form = () => {
     
     const formRef = useRef(null)
-    const onAdd = (event)=>{
+    const onAdd = async(event)=>{
         event.preventDefault();
         if(title && message){
+            const noteFromForm = {
+                title,
+                message,
+                done:false
+            }
+        
+            let noteSavedPromise = await fetch(`http://localhost:8081/api/save/note`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(noteFromForm)
+            })
+            
+            let noteSaved = await noteSavedPromise.json();
             dispatch({
                 type: 'add-note',
-                payload: {
-                    title,
-                    message
-                }
+                payload: noteSaved
             })
 
             formRef.current.reset();
